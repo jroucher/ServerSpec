8213fdb615b7715b8351975231e4ee0b7dc47851
Javier Roucher <jroucher@gmail.com>
2015-09-29 15:59:36 +0000

    Añadiendo pruebas a los templates(MUY INESTABLE)

--- windows/spec/192.168.33.20/sample_spec.rb
+++ windows/spec/192.168.33.20/sample_spec.rb
@@ -26,3 +26,23 @@ end
 describe port(80) do
   it { should be_listening }
 end
+
+describe "distro" do
+  puts "La distro empleada es: #{host_inventory['platform']}"
+end
+
+#describe "memtotal" do
+#  puts "Memoria Total: #{host_inventory['memory']['total']}"
+#end
+
+#describe host('win-2012r2') do
+#  its(:ipaddress) { should eq '192.168.33.20' } 
+#end
+
+#describe iis_website('WIN-2012R2') do
+#  it { should exist }
+#  it { should be_enabled }
+#  it { should be_running }
+#  it { should have_physcal_path('C:\\inetpub\\www') } 
+#end
+
