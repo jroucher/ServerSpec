1f2f2fea28fa570c35102a737f0d530d4acd483c
root <root@NoBorrar-001241.(none)>
2015-10-15 10:10:40 -0500

    Conexion a windows con winrm funcionando

---
+++ windows/spec/172.24.2.12/windows_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+require 'specinfra'
+
+describe "Operating System" do
+  distro = host_inventory['platform'].to_s
+  puts "La distro empleada es: #{distro}"
+  describe distro do
+    it {distro.should match "windows"}
+  end
+
+  describe command('WMIC LOGICALDISK where drivetype=3 get size') do
+    its(:stdout) {should match /Size(.|\n|\r)*53316939776/ }
+  end
+
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  describe command('& ipconfig') do
+    its(:stdout) { should match /.172\.24\.2\.12/ }
+  end
+
+  describe command('WMIC memorychip get capacity') do
+    its(:stdout) {should match /(.|\n|\r)*1073741824/ }
+  end
+end
+
+
+#describe "Memory" do
+##  pending
+##  puts "Memoria Total: #{host_inventory['memory']['total']}"
+##  it "Total memory" do
+##    host_inventory['memory']['total'].to_i.should >= 1000000
+##  end 
+#end
+
+
+#describe iis_website('Default Website') do
+#  it { should exist }
+#  it { should be_enabled }
+#  it { should be_running }
+#  it { should have_physcal_path('C:\\inetpub\\www') } 
+#end
+
+
