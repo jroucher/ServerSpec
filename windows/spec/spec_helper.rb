a5612985efb3c09f21527d0c2bc3d2477a269eaa
Javier Roucher <jroucher@gmail.com>
2015-11-04 05:40:46 -0500

    borrando fichero no necesarios

--- windows/spec/spec_helper.rb
+++ windows/spec/spec_helper.rb
@@ -1,13 +1,31 @@
 require 'serverspec'
 require 'winrm'
+#require 'specinfra'
+
+#include SpecInfra::Helper::WinRM
+#include SpecInfra::Helper::Windows
+
+#RSpec.configure do |c|
+#  c.host  = ENV['TARGET_HOST']
+#  set_property properties[c.host]
+#
+#  user = "administrador"
+#  pass = "VMware01!"
+#  endpoint = "http://#{c.host}:5985/wsman"
+#
+#  c.winrm = ::WinRM::WinRMWebService.new(endpoint, :ssl, :user => user, :pass => pass, :basic_auth_only => true)
+#  c.winrm.set_timeout 30
+#end
 
 set :backend, :winrm
 set :os, :family => 'windows'
 
-user = "vagrant"
-pass = "vagrant"
-endpoint = "http://#{ENV['TARGET_HOST']}:5985/wsman"
+user = 'administrador'
+pass = 'VMware01!'
+host = ENV['TARGET_HOST']
+endpoint = "http://#{host}:5985/wsman"
+puts ("Estableciendo conexion a < #{endpoint} >")	# DEBUGIN
 
 winrm = ::WinRM::WinRMWebService.new(endpoint, :ssl, :user => user, :pass => pass, :basic_auth_only => true)
-winrm.set_timeout 300 # 5 minutes max timeout for any operation
+winrm.set_timeout 300 #  30 seconds max timeout for any operation
 Specinfra.configuration.winrm = winrm
