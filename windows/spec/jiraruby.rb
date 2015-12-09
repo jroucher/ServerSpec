1e867da3270c3586db91f245288c0bba70181ed1
Javier Roucher <jroucher@gmail.com>
2015-12-09 13:00:13 -0500

    puerto, mensaje error

---
+++ windows/spec/jiraruby.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'pp'
+
+class JiraRuby
+  
+  @client = nil
+  
+  def initialize
+    username = "vdc.ruby"
+    password = "VDC.ruby"
+    options = {
+              :username  => username,
+              :password  => password,
+              #:site      => 'https://jirapdi.tid.es',
+              :site      => 'https://pre-epg-jira-01',
+  	      :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
+              :context_path => '',
+              :auth_type => :basic
+            } 
+    @client = JIRA::Client.new(options)
+  end
+
+  def create(project, parentId, summary, description)
+    issue2 = @client.Issue.build
+    result=issue2.save({"fields"=>{
+    		"parent" => {"id" => parentId.to_s},
+    		"summary" => summary,
+  		"description" =>  description,
+    		"project" => {"id" => project.to_s},
+    		"issuetype" => {"id" => "13"}
+    	    }})
+    if result
+      puts "Execution '#{summary}' create successfully"
+    else
+      puts "Execution '#{summary}' produce error in creation"
+    end
+    issue2.fetch
+  end #def
+end #class  
